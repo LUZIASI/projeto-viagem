ec83df149e0e25404395714b9d9c0c9fdf855f21
LUZIASI <luzia.s.rocha@aluno.sneai.br>
2026-04-06 11:01:09 -0500

    subindo atvdd

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {Menu} from './menu/menu'
+import {Menu} from './menu/menu';
+import {Locais} from './locais/locais';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Menu],
+  imports: [RouterOutlet, Menu, Locais],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('viagem');
-}
+}''
